46664fa49535792701d8d668f00b6def2dd34ddf
fadhlan <fadhlan@weekendinc.com>
2022-07-21 10:56:56 +0700

    refactor

---
+++ _sdk/src/main/java/com/otto/sdk/OttoActivity.java
@@ -0,0 +1,8 @@
+package com.otto.sdk;
+
+import android.app.Activity;
+
+
+public abstract class OttoActivity extends Activity {
+  abstract void saldoUpdate(String saldo);
+}
---
+++ _sdk/src/main/java/com/otto/sdk/OttoApplication.java
@@ -0,0 +1,7 @@
+package com.otto.sdk;
+
+
+import android.app.Application;
+
+public interface OttoApplication {
+}
